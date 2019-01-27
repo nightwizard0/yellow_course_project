c0b492ed4868cdcf698b3fe9bc6eba5af711662d
NightWizard <mrenotenot@gmail.com>
2019-01-27 21:21:51 +0300

    Сделан класс Date

--- date.cpp
+++ date.cpp
@@ -1,1 +1,34 @@
+#include <string>
+#include <stdexcept>
+#include <iomanip>
+
 #include "date.h"
+
+
+Date ParseDate(istream& is)
+{
+    string s_year, s_month;
+
+    getline(is, s_year, '-');
+    getline(is, s_month, '-');
+
+    int day, month, year;
+    is >> day;
+
+    month = stoi(s_month);
+    year = stoi(s_year);
+
+    if (day < 1 || day > 31 || month < 1 || month > 12)
+        throw invalid_argument("Invalid date: " + s_year + "-" + s_month + "-" + to_string(day));
+
+    return Date(year, month, day);
+}
+
+ostream& operator<<(ostream& os, const Date& date)
+{
+    os << setfill('0') << setw(4) << date.Year << '-';
+    os << setfill('0') << setw(2) << date.Month << '-';
+    os << setfill('0') << setw(2) << date.Day;
+
+    return os;
+}
--- date.h
+++ date.h
@@ -1,1 +1,17 @@
 #pragma once
+
+#include <iostream>
+
+using namespace std;
+
+class Date
+{
+public:
+    Date(int year, int month, int day) : Year(year), Month(month), Day(day)
+    {}
+
+    const int Year, Month, Day;
+};
+
+Date ParseDate(istream& is);
+ostream& operator<<(ostream& os, const Date& date);
